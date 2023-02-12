f13f434ba80fb238ab510d128d1bda0884c0e42b
Aniruddha <aniruddhavikharankarfb@gmail.com>
2023-02-12 13:48:10 +0530

    create profile changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -485,7 +485,7 @@ const Navbar = ({ connectToContract, userAddress }) => {
                         </Link>
 
                         <a
-                          href="#"
+                          href="/create/create-profile"
                           class="flex items-center p-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
                         >
                           <svg
--- src/pages/create/create-profile.jsx
+++ src/pages/create/create-profile.jsx
@@ -3,76 +3,106 @@ import React from "react";
 const CreateProfile = () => {
   return (
     <div className="h-[100vh] bg-[#111827] pt-6">
-      <section className="max-w-4xl p-6 mx-auto my-20 bg-white rounded-md shadow-md dark:bg-gray-800">
-        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
-          Create Profile
-        </h2>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="flex justify-center min-h-screen">
+          <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+            <div class="w-full">
+              <h1 class="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
+                Create your account
+              </h1>
 
-        <form>
-          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-            <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="username"
-              >
-                Username
-              </label>
-              <input
-                id="username"
-                type="text"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-              />
-            </div>
+              <p class="mt-4 text-gray-500 dark:text-gray-400">
+                Get started on quicklance by creating your account
+              </p>
 
-            <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="emailAddress"
-              >
-                Email Address
-              </label>
-              <input
-                id="emailAddress"
-                type="email"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-              />
-            </div>
+              {/* <div class="mt-6">
+                <h1 class="text-gray-500 dark:text-gray-300">Who are you ?</h1>
 
-            <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                id="password"
-                type="password"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-              />
-            </div>
+                <div class="mt-3 md:flex md:items-center md:-mx-2">
+                  <button class="flex justify-center w-full px-6 py-3 text-white bg-blue-500 rounded-lg md:w-auto md:mx-2 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
 
-            <div>
-              <label
-                className="text-gray-700 dark:text-gray-200"
-                htmlFor="passwordConfirmation"
-              >
-                Password Confirmation
-              </label>
-              <input
-                id="passwordConfirmation"
-                type="password"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
-              />
+                    <span class="mx-2">
+                      Employer
+                    </span>
+                  </button>
+
+                  <button class="flex justify-center w-full px-6 py-3 mt-4 text-blue-500 border border-blue-500 rounded-lg md:mt-0 md:w-auto md:mx-2 dark:border-blue-400 dark:text-blue-400 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
+
+                    <span class="mx-2">
+                      Freelancer
+                    </span>
+                  </button>
+                </div>
+              </div> */}
+
+              <form class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+                <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Username</label>
+                  <input type="text" name="username" placeholder="@sonu" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                </div>
+                <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
+                  <input type="text" name="fullname" placeholder="Raju Babu" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                </div>
+
+                <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Age</label>
+                  <input type="number" name="age" placeholder="21" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                </div>
+
+                {/* <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
+                  <input name="email" type="email" placeholder="johnsnow@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                </div> */}
+
+                <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Who are you ?</label>
+                  <select class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" name="identity" id="identity">
+                    <option value="freelancer">Freelancer</option>
+                    <option value="employer">Employer</option>
+                    <option value="company">Company</option>
+                  </select>
+                </div>
+
+                <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">About You</label>
+                  <textarea name="about" cols="20" rows="1" placeholder="Write a few lines about you" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"></textarea>
+                </div>
+
+                <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Twitter URL</label>
+                  <input name="twitter" type="text" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                </div>
+                <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Github URL</label>
+                  <input name="github" type="text" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                </div>
+                <div>
+                  <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Linkedin URL</label>
+                  <input name="linkedin" type="text" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                </div>
+
+
+                <button type="submit"
+                  class="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                  <span>Create Profile </span>
+
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 rtl:-scale-x-100" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd" />
+                  </svg>
+                </button>
+              </form>
             </div>
           </div>
-
-          <div className="flex justify-end mt-6">
-            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Save
-            </button>
-          </div>
-        </form>
+        </div>
       </section>
     </div>
   );
